3176850e24c6f014f80bc9c14748f3a5d0a85d03
Ali Baghban <ali.angori007@gmail.com>
2025-02-24 20:02:40 +0330

    feat: added prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-/* eslint-disable */
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import sonarjs from 'eslint-plugin-sonarjs';
-import unusedImports from "eslint-plugin-unused-imports"
-import react from 'eslint-plugin-react'
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import sonarjs from "eslint-plugin-sonarjs";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 /**
  * ESLint configuration.
@@ -15,97 +15,76 @@ import react from 'eslint-plugin-react'
  * @type {import("eslint").Linter.Config}
  */
 export default tseslint.config(
-  tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: ['*.js', '*.mjs']
-        },
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  { ignores: ['dist'] },
+  { ignores: ["dist", "/.cache", "/.git", "/.husky", "/.yarn"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'sonarjs': sonarjs,
-      'unused-imports': unusedImports,
-      react
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
-    ignores: [
-      '/.cache',
-      '/.git',
-      '/.husky',
-      '/.yarn',
-      '/*/dist',
-      // '*/eslint.config.js',
-    ],
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'import/default': 'off',
-      'react/no-children-prop': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'import/no-named-as-default-member': 'off',
-      'import/no-named-as-default': 'off',
-      'sonarjs/no-duplicate-string': 'off',
-      'sonarjs/cognitive-complexity': ['warn', 50],
-      'sonarjs/prefer-immediate-return': 'warn',
-      complexity: ['warn', { max: 20 }],
-      'max-depth': 'error',
-      'max-params': 'error',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'warn',
-      '@typescript-eslint/prefer-nullish-coalescing': [
-        'warn',
-        {
-          ignoreConditionalTests: true,
-        },
-      ],
-      'react/jsx-curly-brace-presence': 'warn',
-      'react/jsx-boolean-value': 'warn',
-      'react/jsx-max-depth': ['warn', { max: 8 }],
-      'react/no-array-index-key': 'warn',
-      'react/jsx-pascal-case': 'warn',
-      'react/self-closing-comp': 'warn',
-      'import/no-duplicates': 'off',
-      '@typescript-eslint/consistent-type-imports': [
-        'warn',
-        {
-          fixStyle: 'inline-type-imports',
-        },
-      ],
-      // https://tkdodo.eu/blog/array-types-in-type-script
-      '@typescript-eslint/array-type': [
-        'error',
-        {
-          default: 'generic',
-        },
-      ],
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-        },
-      ],
     },
   },
   {
-    files: ['*/eslint.config.js', '*/vite.config.ts'],
-    env: { node: true },
+    files: ["*.ts", "*.tsx"],
+    plugins: {
+      sonarjs,
+      "unused-imports": unusedImports,
+      react,
+      prettier,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "import/default": "off",
+      "react/no-children-prop": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "import/no-named-as-default-member": "off",
+      "import/no-named-as-default": "off",
+      "sonarjs/no-duplicate-string": "off",
+      "sonarjs/cognitive-complexity": ["warn", 50],
+      "sonarjs/prefer-immediate-return": "warn",
+      complexity: ["warn", { max: 20 }],
+      "max-depth": "error",
+      "max-params": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "react/jsx-curly-brace-presence": "warn",
+      "react/jsx-boolean-value": "warn",
+      "react/jsx-max-depth": ["warn", { max: 8 }],
+      "react/no-array-index-key": "warn",
+      "react/jsx-pascal-case": "warn",
+      "react/self-closing-comp": "warn",
+      "import/no-duplicates": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      // https://tkdodo.eu/blog/array-types-in-type-script
+      "@typescript-eslint/array-type": [
+        "error",
+        {
+          default: "generic",
+        },
+      ],
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
   },
 );
